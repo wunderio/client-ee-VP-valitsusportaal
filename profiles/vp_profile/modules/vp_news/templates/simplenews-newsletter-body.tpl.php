e7e4233b0af5e7b9d83a34aa841671e25ea54567
Ra Mänd <ram4nd@gmail.com>
2014-06-09 11:45:21 +0300

    Integrate newsletter functionality.

--- profiles/vp_profile/modules/vp_news/templates/simplenews-newsletter-body.tpl.php
+++ profiles/vp_profile/modules/vp_news/templates/simplenews-newsletter-body.tpl.php
@@ -45,7 +45,7 @@ $created_custom = format_date($build['#node']->created, 'medium');
 <div align="center" style="background: #f6f5ef;">
   <table bgcolor="#fff" width="600" cellpadding="0" cellspacing="0" border="0" style="border: 0; border: 0 !important; background-color: #fff; width: 600px;">
     <tr>
-      <td style="border: 0; border: 0 !important; font-size: 24px; color: #444; line-height: 24px; font-family: Tahoma, Verdana, Segoe, sans-serif; padding: 40px 0 20px 45px; "><?php print variable_get('site_name'); ?></td>
+      <td style="border: 0; border: 0 !important; font-size: 24px; color: #444; line-height: 24px; font-family: Tahoma, Verdana, Segoe, sans-serif; padding: 40px 0 20px 45px; "><img src="<?php print theme_get_setting('logo', 'vp_theme'); ?>" alt=""></td>
     </tr>
     <tr>
       <td align="left" bgcolor="#fff" style="background-color: #fff; border: 0; border: 0 !important; font-family: Tahoma, Verdana, Segoe, sans-serif; font-size: 13px; line-height: 20px; color: #333; padding: 0; margin: 0;">
@@ -53,7 +53,6 @@ $created_custom = format_date($build['#node']->created, 'medium');
           <h2 style="color: #00668C; font-size: 20px; line-height: 36px; font-weight: normal; text-shadow: 0 0 4px rgba(0, 0, 0, 0.18);"><?php print $title; ?></h2>
           <div style="color: #686868; font-size: 11px; margin: 0 0 15px 0;"><?php print $created_custom; ?></div>
           <?php print render($build); ?>
-          <div style="margin: 15px 0 0 0;"><strong><?php print t('Subjects') ?>:</strong> <?php print $subjects_custom; ?></div>
         </div>
       </td><!--Content-->
     </tr>
