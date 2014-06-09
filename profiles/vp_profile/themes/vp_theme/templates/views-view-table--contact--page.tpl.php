aa880a25f34293db20cdfd0930944c5c16e06850
Hannes Kirsman <hannes.kirsman@eesti.ee>
2014-06-09 12:09:41 +0300

    Fix validation errors on contact page.

--- profiles/vp_profile/themes/vp_theme/templates/views-view-table--contact--page.tpl.php
+++ profiles/vp_profile/themes/vp_theme/templates/views-view-table--contact--page.tpl.php
@@ -80,7 +80,7 @@ foreach ($rows as &$row) {
               $node_view['field_profile_photo']['#items'][0]['is_default'] === TRUE
             )
           ) {
-            $node_view['field_profile_photo'] = array('#markup' => '<div class="field field-name-field-profile-photo field-type-image field-label-hidden"><img src="/profiles/vp_profile/modules/vp_contact/no-profile-image_w104.png" /></div>');
+            $node_view['field_profile_photo'] = array('#markup' => '<div class="field field-name-field-profile-photo field-type-image field-label-hidden"><img alt="" src="/profiles/vp_profile/modules/vp_contact/no-profile-image_w104.png" /></div>');
           }
           $node_view = render($node_view);
         }
