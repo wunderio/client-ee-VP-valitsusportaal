aefff49d39f25453222f1892de538ded0d23ccab
Aljona Orlova <aljona.orlova@bpw-consulting.com>
2017-05-26 15:56:41 +0300

    VP tables

---
+++ profiles/vp_profile/themes/vp_theme/js/vp_tables.js
@@ -0,0 +1,23 @@
+/**
+ * VP Tables.
+ */
+(function($) {
+
+    $('.slide-row').each(function() {
+        $(this).find('td::first').prepend('<img src="/profiles/vp_profile/themes/vp_theme/img/10-global/chevron-down.png" class="toggle-icon down" title="chevron-down">')
+        $(this).find('td::first').prepend('<img src="/profiles/vp_profile/themes/vp_theme/img/10-global/chevron-up.png" class="toggle-icon up hidden" title="chevron-up">')
+        $(this).nextUntil('.slide-content-last-row + *').hide();
+    });
+
+    $('.slide-row').on('click', function() {
+        $(this).nextUntil('.slide-content-last-row + *').toggle();
+        $(this).find('.toggle-icon.down').toggleClass('hidden');
+        $(this).find('.toggle-icon.up').toggleClass('hidden');
+    })
+
+    $("table.vp-table").on("scroll", function() {
+        $(this).find('*').width($(this).width() + $(this).scrollLeft());
+    });
+
+
+})(jQuery);
